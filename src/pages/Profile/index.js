da3a4a7a3d124182bd3e1ef09ad90c851ec8e41e
Daniel <daniel.rosendos@hotmail.com>
2020-02-24 23:22:57 -0300

    Ajustes da aula

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,43 @@
+import { Form, Input } from '@rocketseat/unform';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu Endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua Senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova Senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação da Nova Senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+
+      <button type="button">Sair do Gobarber</button>
+    </Container>
+  );
 }
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -6,7 +6,7 @@ import history from '~/services/history';
 
 import { signInSuccess, signFailure } from './actions';
 
-export function* sigIn({ payload }) {
+export function* signIn({ payload }) {
   try {
     const { email, password } = payload;
 
@@ -22,6 +22,8 @@ export function* sigIn({ payload }) {
       return;
     }
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -31,4 +33,36 @@ export function* sigIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', sigIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no Cadastro verifique seus dados!');
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
